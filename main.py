f783a0a93bfa869e1123ce16a44179c975572c43
evingerbono <bono0823@gmail.com>
2023-01-31 11:37:52 +0100

    eredmenyekm_tesztelés_és_vizsgálata_ha mind2>21

--- main.py
+++ main.py
@@ -8,12 +8,18 @@ def pontok(lapok):
 def eredmeny(gep_lapok,jatekos_lapok):
     gep_pont=pontok(gep_lapok)
     jatekos_pont=pontok(jatekos_lapok)
-    if gep_pont>21:
-        print("A gép vesztett.")
+    if gep_pont>21 and jatekos_pont>21:
+        return "Mindenki vesztett"
+    elif gep_pont>21:
+        return "A gép vesztett."
     elif jatekos_pont>21:
-        print("A játékos vesztett.")
+        return "A játékos vesztett."
 """Tesztestek"""
 
+print(eredmeny([1,2,8,5],[7,6,10]))
+print(eredmeny([1,2,8,5],[4,6,10]))
+print(eredmeny([1,2,8,5,10],[4,6,10,]))
+print(eredmeny([1,2,8,5,10],[4,6,10,10]))
 
 
 
