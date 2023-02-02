d5c996a43abad8c5a6a2dd527744d04924530c84
evingerbono <bono0823@gmail.com>
2023-02-02 15:47:18 +0100

    jatekos_egyenlő_lapertek_teszt

--- main.py
+++ main.py
@@ -13,18 +13,29 @@ def eredmeny(gep_lapok,jatekos_lapok):
     elif gep_pont>21:
         return "A gép vesztett."
     elif jatekos_pont>21:
-        return "A játékos vesztett."
+        return "A játékos vesztett"
 
 """Tesztestek"""
 
 def teszt_estek():
-    jatekos_vesztett()
+    #jatekos_vesztett()
+    jatekos_egyenlő_lapertek_teszt()
 def jatekos_vesztett():
-    jatekos=[7, 6, 8]
+    jatekos=[7, 6, 10]
     gep=[1, 2, 8, 5]
     vart_eredmeny="A játékos vesztett"
     kapott_eredmeny=eredmeny(gep,jatekos)
-    if vart_eredmeny==kapott_eredmeny:
+    if vart_eredmeny == kapott_eredmeny:
+        print(f"{vart_eredmeny} teszt sikeres volt.")
+    else:
+        print(f"{vart_eredmeny} teszt nem volt sikeres.")
+
+def jatekos_egyenlő_lapertek_teszt():
+    jatekos = [2,3,5,4,8]
+    gep = [10,6,7]
+    vart_eredmeny = "A játékos vesztett több lappal"
+    kapott_eredmeny = eredmeny(gep, jatekos)
+    if vart_eredmeny == kapott_eredmeny:
         print(f"{vart_eredmeny} teszt sikeres volt.")
     else:
         print(f"{vart_eredmeny} teszt nem volt sikeres.")
